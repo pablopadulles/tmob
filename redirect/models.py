68a53b29b05f70600673ed351c9e2dcee9606dda
Pablo <pablopadulles@gmail.com>
2022-07-26 20:05:59 -0300

    Arreglo POST data

--- redirect/models.py
+++ redirect/models.py
@@ -5,6 +5,6 @@ class Redirect(models.Model):
     key = models.CharField(max_length=40, blank=False, null=False)
     url = models.TextField()
     active = models.BooleanField(default=True)
-    update_at = models.DateTimeField()
-    created_at = models.DateTimeField()
+    update_at = models.DateTimeField(blank=True, null=True)
+    created_at = models.DateTimeField(null=True)
 
--- redirect/views.py
+++ redirect/views.py
@@ -2,21 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from .models import Redirect
 from datetime import datetime
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
+def add(request):
+    if request.method == 'POST':
+        print(request.POST)
+        url = request.POST['url']
+        key = request.POST['key']
+        active = request.POST['active']
+        redirect = Redirect(key=key, url=url, active=active, created_at=datetime.now())
+        redirect.save()
+        return JsonResponse({'status':'ok'})
 
-def add_redirec(request):
-    url = request.POST['url']
-    key = request.POST['key']
-    active = request.POST['active']
-    redirect = Redirect(key=key, url=url, active=active, created_at=datetime.now())
-    redirect.save()
-    return JsonResponse({'status':'ok'})
-
-def update_redirec(request, id):
-    redirect = Redirect.objects.get(id=id)
-    redirect.url = request.POST['url']
-    redirect.key = request.POST['key']
-    redirect.active = request.POST['active']
-    redirect.update_at = datetime.now()
-    redirect.save()
-    return JsonResponse({'status':'ok'})
+@csrf_exempt
+def update(request, id):
+    if request.method == 'POST':
+        redirect = Redirect.objects.get(id=id)
+        redirect.url = request.POST['url']
+        redirect.key = request.POST['key']
+        redirect.active = request.POST['active']
+        redirect.update_at = datetime.now()
+        redirect.save()
+        return JsonResponse({'status':'ok'})
