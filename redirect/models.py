be2349d04f5c71a6ec829b082e515b02eee6fddf
Pablo <pablopadulles@gmail.com>
2022-07-26 23:34:44 -0300

    Agrega memcached, y se crea signal post_save

--- redirect/models.py
+++ redirect/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.db.models import signals
+from django.dispatch import receiver  
+from django.core.cache import cache
 
 # Create your models here.
 class Redirect(models.Model):
@@ -8,3 +11,8 @@ class Redirect(models.Model):
     update_at = models.DateTimeField(blank=True, null=True)
     created_at = models.DateTimeField(null=True)
 
+@receiver(signals.post_save, sender=Redirect)
+def create_redirec(sender, instance, created, **kwargs) -> None:
+    redirect = Redirect.objects.filter(active=True).values()
+    for redi in redirect:
+        cache.set(redi.get('id'), {'key':redi.get('key'), 'url':redi.get('url')})
--- tmob/settings.py
+++ tmob/settings.py
@@ -125,3 +125,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
